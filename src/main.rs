d7d86d809727c0a4c7c096bed3c907729b7ac935
Mott's Applesauce <josiahvega123@gmail.com>
2022-06-28 14:33:26 -0400

    removed auth

--- src/main.rs
+++ src/main.rs
@@ -42,8 +42,6 @@ fn main() -> Html {
             <Nav>
                 <ul class={"navbar-ul"}>
                     <NavItem><a class="img" href={"/"}><img id={ "logo" } src={ "https://cdn.discordapp.com/attachments/965680606962929707/965792629835038760/Harmonious_Logo_-_PREVIEW_VERSION.png" } width={ "47" } height= { "47" }/></a></NavItem>
-                    <NavItem><a class="nav" href={"/login"}><Title level={Level::H3} size={Size::XLarge}><b>{"Login"}</b></Title></a></NavItem>
-                    <NavItem><a class="nav" href={"/signup"}><Title level={Level::H3} size={Size::XLarge}><b>{"Sign Up"}</b></Title></a></NavItem>
                     <NavItem><a class="nav" href={"/download"}><Title level={Level::H3} size={Size::XLarge}><b>{"Download"}</b></Title></a></NavItem>
                     <NavItem><a class="nav" href={"/about"}><Title level={Level::H3} size={Size::XLarge}><b>{"About"}</b></Title></a></NavItem>
                 </ul>
